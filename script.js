bac16dee06f42ac9559d8a9c8a2e52d445a2e589
abhijith1998 <53164596+abhijith1998@users.noreply.github.com>
2020-05-17 10:02:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//ES6
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#camera')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
+   Quagga.onDetected(function(data){
+    document.getElementById('result').innerHTML=data.codeResult.code;
+    return;
+   });
+//jquery
+
+
+
+
+
+
+//ajax
+var xhr= new XMLHttpRequest()
